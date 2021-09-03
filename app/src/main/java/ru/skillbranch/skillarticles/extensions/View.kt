1c020f135c8cb53e4760cc322220ad0c45f032b1
kkurtukov <konstantinkg@gmail.com>
2021-09-03 09:39:29 +0600

    -custom view hometask

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -16,3 +16,17 @@ fun View.setMarginOptionally(left: Int = marginLeft, top: Int = marginTop, right
 fun View.setPaddingOptionally(left: Int = 0, top: Int = 0, right: Int = 0, bottom: Int = 0){
     this.setPadding(left, top, right, bottom)
 }
+
+fun List<Pair<Int, Int>>.groupByBounds(bounds: List<Pair<Int, Int>>): List<MutableList<Pair<Int, Int>>>{
+    val results: MutableList<MutableList<Pair<Int, Int>>> = mutableListOf()
+    bounds.forEach { bound ->
+        var group = mutableListOf<Pair<Int, Int>>()
+        this.forEach {
+            if (it.first >= bound.first && it.second <= bound.second){
+                group.add(it)
+            }
+        }
+        results.add(group)
+    }
+    return results
+}
